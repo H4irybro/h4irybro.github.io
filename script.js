b30f20756bfae31e78512f14c83a344065cca23e
H4irybro <hairy.bro@gmail.com>
2016-12-30 01:35:01 +0000

    script

--- script.js
+++ script.js
@@ -16,7 +16,6 @@ function Post(name, date, text)
   document.write("<div class='posts' style='border: solid 1px black'>");
   document.write("<table><tr><td>");
   if (name == "kek") { name = "{SUPER_USER} " + name; }
-  if (name == "mutt") { name = "{ADMIN} " + name; }
   if (text.split(" ")[0] == "[img]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[IMAGE POST]</p><img style='width:200px;width:200px;' src='" + text.split(" ")[1] + "' /></div>"; }
   if (text.split(" ")[0] == "[link]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[LINK POST]</p><a href='"+ text.split(" ")[1] + "'>" + text.split("/")[2] + "</a></div>"; }
   if (text.split(" ")[0] == "[YouTube]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[YouTube POST]</p><iframe width='560' height='315' src='"+ text.split(" ")[1] + "' frameborder='0'></iframe></div>"; }
