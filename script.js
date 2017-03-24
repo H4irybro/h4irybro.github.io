b845622aea71ed64385196b020d5f9c34430c406
H4irybro <hairy.bro@gmail.com>
2017-03-24 23:55:21 +0000

    stuff, and other stuff

--- script.js
+++ script.js
@@ -2,14 +2,18 @@ var filtering = false;
 var postCount = 0;
 
 var profiles = {
-  "kek": ""
+  "kek":  "user_profiles\\kek.jpg",
+  "mutt": "user_profiles\\mutt.jpg",
+  "paddy":"user_profiles\\paddy.jpg"
 };
 
 function Post(name, date, text)
 {
   postCount+=1;
+  
   var filt = findGetParameter("filter");
   filtering = true;
+  
   if (filt != null)
   {
 	  if (filt != name) 
@@ -17,14 +21,28 @@ function Post(name, date, text)
 		  return;
 	  }
   }
+  
   document.write("<div class='posts' style='border: solid 1px black'>");
-  document.write("<table><tr><td>");
-  if (text.split(" ")[0] == "[img]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[IMAGE POST]</p><img style='width:20%;width:20%;' src='" + text.split(" ")[1] + "' /></div>"; }
-  if (text.split(" ")[0] == "[link]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[LINK POST]</p><a href='"+ text.split(" ")[1] + "'>" + text.split("/")[2] + "</a></div>"; }
-  if (text.split(" ")[0] == "[YouTube]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[YouTube POST]</p><iframe width='560' height='315' src='"+ text.split(" ")[1] + "' frameborder='0'></iframe></div>"; }
-  //if ((date.split(".")[0] == d.getDate()) && (date.split(".")[1] == d.getMonth()) && (date.split(".")[2] == d.getFullYear())) { date = "Today"; }
-  document.write("<p id='name_date'><strong><span style='border: solid 1px black;'>" + name + " @ " + date + "</span></strong></p></td></tr></table>");
-  document.write("<p><span style='font-family:arial;'>" + text + "</span></p></div>");
+  document.write("<table><tr><td id=\"tdstuff\">");
+
+  if (text.split(" ")[0] == "[img]") 
+    {
+      text = "<div><img style='width:20%;width:20%;' src='" + text.split(" ")[1] + "' /></div>"; 
+    }
+  if (text.split(" ")[0] == "[link]") 
+    { 
+      text = "<div><a href='"+ text.split(" ")[1] + "'>" + text.split("/")[2] + "</a></div>";
+    }
+  if (text.split(" ")[0] == "[YouTube]") 
+    { 
+      text = "<div><iframe width='560' height='315' src='"+ text.split(" ")[1] + "' frameborder='0'></iframe></div>"; 
+    }
+
+  document.write("<div><p id='name_date'>" + name + " @ " + date + "</p><img id=\"profile-image\" src=" + profiles[name] + " /></td>");
+  document.write("<td><div style=\"width:100px;\"></div></div></td>"); // spacer
+  document.write("<td><p style=\"font-family:sans-serif;\">" + text + "</p></td></table></div>");
+  document.write("<div style=\"height: 35px;\"></div>");
+  
   console.log("created post by "+ name +" with id: " + postCount);
 }
 
@@ -47,14 +65,25 @@ function PostMore(name, date, textlines) // for code
   }
 
   document.write("<div class='posts' style='border: solid 1px black'>");
-  document.write("<table><tr><td>");
-  if (text.split(" ")[0] == "[img]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[IMAGE POST]</p><img style='width:200px;width:200px;' src='" + text.split(" ")[1] + "' /></div>"; }
-  if (text.split(" ")[0] == "[link]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[LINK POST]</p><a href='"+ text.split(" ")[1] + "'>" + text.split("/")[2] + "</a></div>"; }
-  if (text.split(" ")[0] == "[YouTube]") { text = "<div><p style='border: solid 1px black;text-align:center;font-size:10px;'>[YouTube POST]</p><iframe width='560' height='315' src='"+ text.split(" ")[1] + "' frameborder='0'></iframe></div>"; }
-  //if ((date.split(".")[0] == d.getDate()) && (date.split(".")[1] == d.getMonth()) && (date.split(".")[2] == d.getFullYear())) { date = "Today"; }
-  document.write("<p id='name_date'><strong><span style='border: solid 1px black;'>" + name + " @ " + date + "</span></strong></p></td></tr></table>");
-  document.write("<p><span style='font-family:arial;'>" + text + "</span></p></div>");
-  console.log("created post by "+ name +" with id: " + postCount);
+  document.write("<table><tr><td id=\"tdstuff\">");
+
+  if (text.split(" ")[0] == "[img]") 
+    {
+      text = "<div><img style='width:20%;width:20%;' src='" + text.split(" ")[1] + "' /></div>"; 
+    }
+  if (text.split(" ")[0] == "[link]") 
+    { 
+      text = "<div><a href='"+ text.split(" ")[1] + "'>" + text.split("/")[2] + "</a></div>";
+    }
+  if (text.split(" ")[0] == "[YouTube]") 
+    { 
+      text = "<div><iframe width='560' height='315' src='"+ text.split(" ")[1] + "' frameborder='0'></iframe></div>"; 
+    }
+
+  document.write("<div><p id='name_date'>" + name + " @ " + date + "</p><img id=\"profile-image\" src=" + profiles[name] + " /></td>");
+  document.write("<td><div style=\"width:100px;\"></div></div></td>"); // spacer
+  document.write("<td><p><span style='font-family:arial;'>" + text + "</span></p></td></table></div>");
+  document.write("<div style=\"height: 35px;\"></div>");
 }
 
 
